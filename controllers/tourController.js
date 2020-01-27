7eea82afe96350e31b44e61308a57e1af4ad1d1b
Anshul Anand <ansh.anand@gmail.com>
2020-01-26 21:59:11 -0500

    eslint mods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,7 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
+  //console.log(`Tour id is ${val}`);
 
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -17,14 +17,14 @@ exports.checkID = (req, res, next, val) => {
 };
 
 exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Missing name or price'
-      });
-    }
-    next();
-  };
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -37,9 +37,9 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
+  //console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find(el => el.id == id);
+  const tour = tours.find(el => el.id === id);
 
   if (!tour) {
     res.status(404).json({
@@ -64,7 +64,7 @@ exports.createTour = (req, res) => {
   fs.writeFileSync(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    err => {
+    () => {
       res.status(201).json({
         status: 'success',
         results: tours.length,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -14,6 +14,6 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
