dbd3048a4efc7613c48f555afad6bab8ffc558ec
Anshul Anand <ansh.anand@gmail.com>
2020-02-06 22:35:33 -0500

    add error routine

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,5 +29,24 @@ app.use((req, res, next) => {
 // 3. ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  //alternative1
+  /* res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server`
+  }); */
+
+  //alternative2
+  /* const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err); */
+
+  //alternative3
+  next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT REJECTION! Shutting Down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -15,11 +21,17 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => {
-    console.log('DB connection is successful');
-  });
+  .then(() => console.log('DB connection is successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App has been started on ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting Down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
